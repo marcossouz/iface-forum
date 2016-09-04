31de6e4a020151657c775849e3859493a1759c7e
Eduarda Chagas <etcc@ic.ufal.br>
2016-09-03 23:01:30 -0300

    Add files via upload

---
+++ source/Answers.java
@@ -0,0 +1,58 @@
+package iface_forum;
+
+public class Answers {
+
+	int id;
+	int idType;
+	int idForum;
+	int user;
+	String comment;
+	
+	public Answers(int ID, int IDTYPE, int IDFORUM, int USER, String COMMENT){
+		this.id = ID;
+		this.idType = IDTYPE;
+		this.idForum = IDFORUM;
+		this.user = USER;
+		this.comment = COMMENT;
+	}
+	
+	public void setId(int i){
+		this.id = i;
+	}
+	
+	public void setIdQuestion(int idQues){
+		this.idType = idQues;
+	}
+	
+	public void setIdForum(int idFor){
+		this.idForum = idFor;
+	}
+	
+	public void setUser(int u){
+		this.user = u;
+	}
+	
+	public void setComment(String com){
+		this.comment = com;
+	}
+	
+	public int getId(){
+		return this.id;
+	}
+	
+	public int getIdQuestion(){
+		return this.idType;
+	}
+	
+	public int getIdForum(){
+		return this.idForum;
+	}
+	
+	public int getUser(){		
+		return this.user;
+	}
+	
+	public String getComment(){
+		return this.comment;
+	}
+}
---
+++ source/Questions.java
@@ -0,0 +1,42 @@
+package iface_forum;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Questions extends Topics{
+	
+	static Scanner input = new Scanner(System.in);
+	private ArrayList<AnswersQuestion> answers = new ArrayList<AnswersQuestion>();
+	
+	public Questions(int id, int idF, int user, String title, String description){
+		super(id,idF,user,title,description);
+	}
+	
+	public void printQuestion(int idQuestion,int idCommunity,int idUser){
+		System.out.printf("\n\nTitle: %s\n",this.getTitle());
+		System.out.printf("Creator: %d\n", this.getUser());
+		System.out.printf("Description: %s\n\n",this.getDescription());
+		 if(answers.size()!=0){
+			 System.out.println("Comments\n");
+			 for(int i=0;i<answers.size();i++){
+				 System.out.printf("User %d\n%s\n",answers.get(i).getUser(),answers.get(i).getComment());				 			 
+			 }
+		}
+		 System.out.print("\n Add Comment (Y/N): ");
+		 String option = input.nextLine();
+		 if(option.equals("Y") || option.equals("y")){
+			 System.out.println("Comment:");
+			 String comment = input.nextLine();
+			 newAnswer(answers.size(),idQuestion,idCommunity,idUser,comment);
+		 }
+		 InterfaceTeste.questionIn(idCommunity, idUser);		
+	}
+	
+	public void newAnswer(int id, int idArticle, int idForum, int user, String comment){
+		AnswersQuestion newAnswer = new AnswersQuestion(id,idArticle,idForum,user, comment);
+		answers.add(newAnswer);
+	}
+	
+	public ArrayList<AnswersQuestion> getAnswers(){
+		return this.answers;
+	}
+}
