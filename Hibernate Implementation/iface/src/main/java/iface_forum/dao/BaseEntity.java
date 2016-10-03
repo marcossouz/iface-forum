a4f21d392d38e89702bf1cbee926d23829b6d121
Lucas Calheiros <lucaslc@ic.ufal.br>
2016-10-02 23:10:48 -0600

    .

--- Hibernate Implementation/iface/src/main/java/iface_forum/dao/BaseEntity.java
+++
@@ -1,5 +0,0 @@
-package iface_forum.dao;
-
-public interface BaseEntity {
-	public Integer getId();
-}
--- Hibernate Implementation/iface/src/main/java/iface_forum/dao/CommunityDAO.java
+++
@@ -1,7 +0,0 @@
-package iface_forum.dao;
-
-import iface_forum.Community;
-
-public class CommunityDAO extends GenericDAO<Community>{
-
-}
